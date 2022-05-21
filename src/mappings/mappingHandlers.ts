0cd0bb4804d0e355ae8730714cf3bbcb22afa291
Alan WANG <alan1995wang@outlook.com>
2022-05-21 12:53:08 +0800

    Fix distribute account typo

--- src/mappings/mappingHandlers.ts
+++ src/mappings/mappingHandlers.ts
@@ -1,134 +1,159 @@
 import { SubstrateEvent } from "@subql/types";
-import { DistributionTx, ClaimTx, TotalClaim, TotalDistribution } from "../types";
+import {
+  DistributionTx,
+  ClaimTx,
+  TotalClaim,
+  TotalDistribution,
+} from "../types";
 
-export const DISTRIBUTION: string = "0x99Ae955aB2a012f0a6605C0359049bf33e5152f0"
+export const DISTRIBUTION: string =
+  "0x323B10Ecf35F287B1dcfe48976cE1078BDA738d0";
 
 type Tx = {
-    id: string, // tx hash
-    from: string,
-    to: string,
-    amount: string,
-    blockHeight: number,
-    timestamp: Date
-}
+  id: string; // tx hash
+  from: string;
+  to: string;
+  amount: string;
+  blockHeight: number;
+  timestamp: Date;
+};
 
 function isDistribution(from: string): boolean {
-    return DISTRIBUTION === from
+  return DISTRIBUTION === from;
 }
 
 function isClaim(to: string): boolean {
-    return DISTRIBUTION === to
+  return DISTRIBUTION === to;
 }
 
-async function handleTotalClaimed(to: string, amount: string, block: number): Promise<void> {
-    logger.info(`handle total claim from[${to}] amount[${amount}] at block[${block}]`)
-    try {
-        let record = await TotalClaim.get(to)
-        if (record === undefined) {
-            record = TotalClaim.create({
-                id: to,
-                amount: "0",
-                blockHeight: block
-            })
-        }
-        record.amount = (BigInt(record.amount) + BigInt(amount)).toString()
-        await record.save()
-    } catch (e: any) {
-        logger.error(`handle account[${to}] total claim error: %o`, e)
-        throw e
+async function handleTotalClaimed(
+  to: string,
+  amount: string,
+  block: number
+): Promise<void> {
+  logger.info(
+    `handle total claim from[${to}] amount[${amount}] at block[${block}]`
+  );
+  try {
+    let record = await TotalClaim.get(to);
+    if (record === undefined) {
+      record = TotalClaim.create({
+        id: to,
+        amount: "0",
+        blockHeight: block,
+      });
     }
+    record.amount = (BigInt(record.amount) + BigInt(amount)).toString();
+    await record.save();
+  } catch (e: any) {
+    logger.error(`handle account[${to}] total claim error: %o`, e);
+    throw e;
+  }
 }
 
-async function handleTotalDistributed(from: string, amount: string, block: number): Promise<void> {
-    logger.info(`handle total distribution to[${from}] amount[${amount}] at block[${block}]`)
-    try {
-        let record = await TotalDistribution.get(from)
-        if (record === undefined) {
-            record = TotalDistribution.create({
-                id: from,
-                amount: "0",
-                blockHeight: block
-            })
-        }
-        record.amount = (BigInt(record.amount) + BigInt(amount)).toString()
-        await record.save()
-    } catch (e: any) {
-        logger.error(`handle account[${from}] total distribution error: %o`, e)
-        throw e
+async function handleTotalDistributed(
+  from: string,
+  amount: string,
+  block: number
+): Promise<void> {
+  logger.info(
+    `handle total distribution to[${from}] amount[${amount}] at block[${block}]`
+  );
+  try {
+    let record = await TotalDistribution.get(from);
+    if (record === undefined) {
+      record = TotalDistribution.create({
+        id: from,
+        amount: "0",
+        blockHeight: block,
+      });
     }
+    record.amount = (BigInt(record.amount) + BigInt(amount)).toString();
+    await record.save();
+  } catch (e: any) {
+    logger.error(`handle account[${from}] total distribution error: %o`, e);
+    throw e;
+  }
 }
 
 async function handleDistribution(tx: Tx): Promise<void> {
-    // logger.info(`handle new distribution from[${tx.from}] to[${tx.to}] amount[${tx.amount}]`)
-    try {
-        let record = await DistributionTx.get(tx.id)
-        if (record !== undefined) {
-            logger.warn(`distribution hash [${tx.id}] has been recorded`)
-            return
-        }
-        record = DistributionTx.create(tx)
-        await Promise.all([
-            record.save(),
-            handleTotalDistributed(tx.from, tx.amount, tx.blockHeight)
-        ])
-    } catch (e: any) {
-        logger.error("handle acala distribution error: %o", e)
-        throw e
+  // logger.info(`handle new distribution from[${tx.from}] to[${tx.to}] amount[${tx.amount}]`)
+  try {
+    let record = await DistributionTx.get(tx.id);
+    if (record !== undefined) {
+      logger.warn(`distribution hash [${tx.id}] has been recorded`);
+      return;
     }
+    record = DistributionTx.create(tx);
+    await Promise.all([
+      record.save(),
+      handleTotalDistributed(tx.from, tx.amount, tx.blockHeight),
+    ]);
+  } catch (e: any) {
+    logger.error("handle acala distribution error: %o", e);
+    throw e;
+  }
 }
 
 async function handleClaim(tx: Tx): Promise<void> {
-    logger.info(`handle new claim from[${tx.from}] to[${tx.to}] amount[${tx.amount}] at ${tx.id}`)
-    try {
-        let record = await ClaimTx.get(tx.id)
-        if (record !== undefined) {
-            logger.warn(`claim hash [${tx.id}] has been recorded`)
-            return
-        }
-        record = ClaimTx.create(tx)
-        await Promise.all([
-            record.save(),
-            handleTotalClaimed(tx.to, tx.amount, tx.blockHeight)
-        ])
-    } catch (e: any) {
-        logger.error("handle acala claim error: %o", e)
-        throw e
+  logger.info(
+    `handle new claim from[${tx.from}] to[${tx.to}] amount[${tx.amount}] at ${tx.id}`
+  );
+  try {
+    let record = await ClaimTx.get(tx.id);
+    if (record !== undefined) {
+      logger.warn(`claim hash [${tx.id}] has been recorded`);
+      return;
     }
+    record = ClaimTx.create(tx);
+    await Promise.all([
+      record.save(),
+      handleTotalClaimed(tx.to, tx.amount, tx.blockHeight),
+    ]);
+  } catch (e: any) {
+    logger.error("handle acala claim error: %o", e);
+    throw e;
+  }
 }
 
-export async function handleTransferEvent(event: SubstrateEvent): Promise<void> {
-    try {
-        const { event: { data: [signer, dest, value] } } = event;
-        const from = signer.toString()
-        const to = dest.toString()
-        const isDistri = isDistribution(from)
-        const isCla = isClaim(to)
-        
-        // filter signer we don't care
-        if (!isDistri && !isCla) {
-            // logger.warn(`ignore event: from[${from}] to[${to.toString()}]`)
-            return
-        }
-        
-        const idx = event.idx
-        const blockHeight = event.block.block.header.number.toNumber()
-        const hash = event.extrinsic.extrinsic.hash.toString()
+export async function handleTransferEvent(
+  event: SubstrateEvent
+): Promise<void> {
+  try {
+    const {
+      event: {
+        data: [signer, dest, value],
+      },
+    } = event;
+    const from = signer.toString();
+    const to = dest.toString();
+    const isDistri = isDistribution(from);
+    const isCla = isClaim(to);
 
-        const tx: Tx = {
-            id: `${hash}-${idx}`,
-            from,
-            to,
-            amount: value.toString(),
-            blockHeight,
-            timestamp: event.block.timestamp
-        }
-        if (isDistri) {
-            await handleDistribution(tx)
-        } else if (isCla) {
-            await handleClaim(tx)
-        }
-    } catch (e: any) {
-        logger.error(`handle transfer event error: %o`, e)
+    // filter signer we don't care
+    if (!isDistri && !isCla) {
+      // logger.warn(`ignore event: from[${from}] to[${to.toString()}]`)
+      return;
     }
-}
 
+    const idx = event.idx;
+    const blockHeight = event.block.block.header.number.toNumber();
+    const hash = event.extrinsic.extrinsic.hash.toString();
+
+    const tx: Tx = {
+      id: `${hash}-${idx}`,
+      from,
+      to,
+      amount: value.toString(),
+      blockHeight,
+      timestamp: event.block.timestamp,
+    };
+    if (isDistri) {
+      await handleDistribution(tx);
+    } else if (isCla) {
+      await handleClaim(tx);
+    }
+  } catch (e: any) {
+    logger.error(`handle transfer event error: %o`, e);
+  }
+}
